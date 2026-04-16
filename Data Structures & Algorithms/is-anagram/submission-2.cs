428fa5c794032474cb5e0a753e1f258a31833ef3
Phat Ngoc <ngocphatc2710@gmail.com>
2026-04-17 01:05:08 +0700

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        if (s.Length != t.Length) return false;
+        int[] chars = new int[26];
+        for (int i = 0; i < s.Length; i++) {
+            chars[s[i] - 'a']++;
+            chars[t[i] - 'a']--;
+        }
+        
+        for (int i = 0; i < chars.Length; i++) {
+            if (chars[i] > 0) return false;
+        }
+
+        return true;
+    }
+}
