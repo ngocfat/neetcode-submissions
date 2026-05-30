177884101cb9b01b990cbd89cfb287f2ac252039
Phat Ngoc <ngocphatc2710@gmail.com>
2026-05-30 21:47:55 +0700

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int Search(int[] nums, int target) {
+       int left = 0;
+       int right = nums.Length - 1; 
+       while(left <= right) {
+          int mid = (right - left) / 2 + left;
+          if (nums[mid] == target) return mid;
+          else if (nums[mid] > target) right = mid - 1;
+          else left = mid + 1;
+       } 
+       return -1;
+    }
+}
